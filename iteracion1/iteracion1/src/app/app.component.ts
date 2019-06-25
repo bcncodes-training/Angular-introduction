9595bd6e82a139e0653566913c3f92b28d996cd8
Raphael <raphaelboury@gmail.com>
2019-06-25 14:26:38 +0200

    done

---
+++ iteracion1/iteracion1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it01-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'National Parks';
+  foto1 = '../assets/np1.jpg';
+  foto2 = '../assets/np2.jpg';
+  foto3 = '../assets/np3.jpg';
+}
