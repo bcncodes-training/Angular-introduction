3a72b979b9f288ac3c46383a5af17ff03c5d8e5b
JCJV2019 <cjordanvergara.outlook.es>
2019-06-26 11:34:52 +0200

    Subido

---
+++ iteracion2/src/app/comments.ts
@@ -0,0 +1,17 @@
+export const comments = [
+  {
+    name: 'Katty',
+    date: '17/06/2019 12:37',
+    msg: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+  },
+  {
+    name: 'Salomon',
+    date:'16/06/2019 18:06',
+    msg: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+  },
+  {
+    name: 'Bondary',
+    date:'16/06/2019 14:37',
+    msg: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
+  }
+];
---
+++ iteracion2/src/app/comments/comments.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { comments } from '../comments';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+  title = 'Comments:';
+  comments = comments;
+  now1 = moment().format('DD/MM/YYYY HH:mm');
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
+
+
