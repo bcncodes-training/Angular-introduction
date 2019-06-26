5508d183d1dfa79783bb9df0bab57e3ef03be9dc
Raphael <raphaelboury@gmail.com>
2019-06-26 11:44:18 +0200

    Final

--- iteracion2/src/app/comments-component/comments-component.component.ts
+++ iteracion2/src/app/comments-component/comments-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import *as moment from 'moment' // busca todo lo que tiene la libreria moment y le asocia el nombre de moment
 
 @Component({
   selector: 'it02-comments-component',
@@ -9,10 +10,14 @@ export class CommentsComponentComponent implements OnInit {
   comments ="Comments"
   user1 = "John"
   commentUser1 = "COMENTARIO NUMERO1"
+  fecha1 : any = moment().format("15-07-2018");
   user2 = "Lisa"
   commentUser2 = "COMENTARIO NUMERO2"
+  fecha2 : any = moment().format("15-06-2018");
   user3 = "Logan"
   commentUser3 = "COMENTARIO NUMERO3"
+  fecha3 : any = moment().format("15-05-2018");
+
 
   constructor() { }
 
