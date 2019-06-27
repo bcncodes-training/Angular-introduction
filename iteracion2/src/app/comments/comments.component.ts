2256ec1293153d5bbf4ce1679c2715d87cd83d7e
Jaume Izquierdo <jaumeizquierdo@gmail.com>
2019-06-27 14:25:35 +0200

    insertar nuevo contenido en el array mediante el formulario

--- iteracion2/src/app/comments/comments.component.ts
+++ iteracion2/src/app/comments/comments.component.ts
@@ -16,11 +16,48 @@ export class CommentsComponent implements OnInit {
   comment3 = 'Lorem ipsum dolor sit amet, consectetur';
   comment4 = '';
 
+  comments: Array<Object> = [
+    {
+      id: 1,
+      name: 'Katy',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in cursus leo',
+      fecha: '25/06/2019'
+    },
+    {
+      id: 2,
+      name: 'Luke',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in cursus leo',
+      fecha: '25/06/2019'
+    },
+    {
+      id: 3,
+      name: 'Han',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in cursus leo',
+      fecha: '25/06/2019'
+    },
+    {
+      id: 4,
+      name: 'Leia',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in cursus leo',
+      fecha: '25/06/2019'
+    }
+  ];
+
+  commentTrackerFunction(index: number, comment: any) {
+    return comment.id;
+  }
+
   fecha:string;
 
   constructor() {
     moment.locale('es');
     this.fecha = moment().format('LLL');
+  }
+
+  addComment(NewComment: string, NewName: string) {
+    const elem = {id: this.comments.length + 1 , name: NewName, comment: NewComment, fecha: this.fecha };
+    //this.comments.push(elem);
+    this.comments = this.comments.concat(elem);
   }
 
   ngOnInit() {
