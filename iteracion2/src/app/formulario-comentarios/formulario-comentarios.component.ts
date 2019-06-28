ad6fe55b7cb4f2998f9318c65a1b561254864474
HectorCo <hcompanyortega@gmail.com>
2019-06-28 10:05:30 +0200

    27062019

--- iteracion2/src/app/formulario-comentarios/formulario-comentarios.component.ts
+++ iteracion2/src/app/formulario-comentarios/formulario-comentarios.component.ts
@@ -9,8 +9,29 @@ export class FormularioComentariosComponent /*implements OnInit*/ {
 
   estado = false;
 
+  comments = [
+    {
+      author: 'Katty',
+      date: ' 25/04/2018 15:26',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum, ligula sit amet dictum blandit, justo purus venenatis nisl, eu facilisis nisi augue vitae ex. Nunc sollicitudin dignissim nibh. In turpis ex, pellentesque eget sodales at, consequat at diam. Nulla egestas lacinia sem, quis imperdiet orci. Nullam pulvinar erat eu.'
+    },
+    {
+      author: 'Salomon',
+      date: ' 13/02/2019 15:26',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum, ligula sit amet dictum blandit, justo purus venenatis nisl, eu facilisis nisi augue vitae ex. Nunc sollicitudin dignissim nibh. In turpis ex, pellentesque eget sodales at, consequat at diam. Nulla egestas lacinia sem, quis imperdiet orci. Nullam pulvinar erat eu.'
+    },
+    {
+      author: 'Bondary',
+      date: ' 10/03/2019 15:26',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum, ligula sit amet dictum blandit, justo purus venenatis nisl, eu facilisis nisi augue vitae ex. Nunc sollicitudin dignissim nibh. In turpis ex, pellentesque eget sodales at, consequat at diam. Nulla egestas lacinia sem, quis imperdiet orci. Nullam pulvinar erat eu.'
+    }
+  ];
+
   mostrarAviso() {
-    alert('Hola');
+    alert(this.comments[0].content);
   }
 
 
