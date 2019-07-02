40c4e3ad9d5c5ac36d28478d98002ce843b91b4a
unknown <you@example.com>
2019-07-02 19:39:12 +0200

    19:39 commit

--- iteracion2/src/app/app.module.ts
+++ iteracion2/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [MainComponent, CommentsComponent]
 })
 export class AppModule { }
---
+++ iteracion2/src/app/comments/comments.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ModuleWithComponentFactories } from '@angular/core';
+
+@Component({
+  selector: 'ite2-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
