8d1a88d4c061b53e9e93ab569213f275077c5902
unknown <you@example.com>
2019-07-05 17:48:14 +0200

    nosevelaputafoto commit

--- iteracion2/src/app/app-routing.module.ts
+++ iteracion2/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommentsComponent } from './comments/comments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/comments', pathMatch: 'full'},
+  { path: 'comments', component: CommentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
