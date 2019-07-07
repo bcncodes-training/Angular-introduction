6d817ea19c93d0d5450a0f7ca714442ca9128b56
vicmonfun <vicmon69@gmx.es>
2019-07-07 19:31:47 +0200

    commit iteracion1

---
+++ iteracion1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it01-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Fondos Marinos';
+  foto1 = '../assets/img1.jpg';
+  foto2 = '../assets/img2.jpg';
+  foto3 = '../assets/img3.jpg';
+}
