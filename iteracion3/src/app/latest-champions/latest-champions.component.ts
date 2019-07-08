9d1c3832d3130b928b2125a9f8633fba5d77114c
vicmonfun <vicmon69@gmx.es>
2019-07-08 20:21:00 +0200

    commit iteracion3 arreaglado

--- iteracion3/src/app/latest-champions/latest-champions.component.ts
+++ iteracion3/src/app/latest-champions/latest-champions.component.ts
@@ -11,20 +11,23 @@ export class LatestChampionsComponent implements OnInit {
   latestChampions: Array<Object> = [{
     year: '2015-16',
     team: 'Cleveland Cavaliers',
-    urlImg: '../assets/cavaliers_logo.jpg',
+    url: 'https://en.wikipedia.org/wiki/2015%E2%80%9316_Cleveland_Cavaliers_season',
+    Image: '../assets/cavaliers_logo.jpg',
     bestPlayers: ['Lebron James', 'Kyrie Irving', 'Kevin Love', 'J.R. Smith', 'Anderson Varejao']
   },
   {
     year: '2014-15',
     team: 'Golden State Warriors',
-    urlImg: '../assets/golden-warriors_logo.png',
+    url: 'https://en.wikipedia.org/wiki/2014%E2%80%9315_Golden_State_Warriors_season',
+    Image: '../assets/golden-warriors_logo.png',
     bestPlayers: ['Stephen Curry', 'Klay Thompson', 'Draymong Green', 'Andrew Bogut', 'Gestus Ezeli']
   },
   {
     year: '2013-14',
     team: 'San antonio Spurs',
-    urlImg: '../assets/san-antonio_logo.png',
-    bestPlayers: ['Tim Duncan', 'Tony Parker', 'Manu Ginóbili', 'Kawhi Leonard', 'Tiago Splitter']
+    url: 'https://en.wikipedia.org/wiki/2013%E2%80%9314_San_Antonio_Spurs_season',
+    Image: '../assets/san-antonio_logo.png',
+    bestPlayers: ['Tim Duncan', 'Tony Parker', 'Manu Ginobili', 'Kawhi Leonard', 'Tiago Splitter']
   }];
 
   constructor() { }
